294b85b9d4595c102839077ed9edca72158da094
Phil Lang <plangphoto@gmail.com>
2017-03-15 14:05:42 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,38 @@
+import csv
+import time
+from datetime import datetime
+from dateutil.parser import parse
+
+
+# Opens read / write
+def clearLines():
+    with open('data/raw.csv', 'rb') as infile, open('data/manifest.csv', 'wt') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+
+        for row in reader:
+            if row[0] != "":
+                writer.writerow(row)
+
+
+# Creates a string and writes to .txt
+def formString():
+    with open('data/manifest.csv', 'rb') as infile, open('data/manifest.txt', 'wt') as outfile:
+        reader = csv.reader(infile)
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+        for row in reader:
+            migrantFirstName = row[0].title()
+            migrantSurName = row[1].title()
+            migrantAge = row[2].replace("age", "aged")
+            migrantCountry = row[3].title()
+            migrantEmbark = row[4].title()
+            migrantDate = datetime.strptime(row[5], "%m/%d/%Y")
+            migrantDate = "{0:} {1:} {2:}".format(months[migrantDate.month - 1], migrantDate.day, migrantDate.year)
+            migrant = migrantFirstName + ' ' + migrantSurName + ', ' + migrantAge + ', from ' + migrantCountry + ', ' + 'arrived in New York on ' + migrantDate + ', having embarked from ' + migrantEmbark + '.\n'
+            outfile.write(migrant)
+
+
+# Call functions
+clearLines()
+formString()
